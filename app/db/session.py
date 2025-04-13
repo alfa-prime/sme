352ba3c479543ef0024f515ec94df004dde30235
alfa <imperium-alfa@proton.me>
2025-04-13 22:38:36 +0300

    Initial commit

---
+++ app/db/session.py
@@ -0,0 +1,21 @@
+from typing import AsyncGenerator
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine  # noqa
+from sqlalchemy.orm import sessionmaker  # noqa
+from app.core.config import get_settings
+
+settings = get_settings()
+
+engine = create_async_engine(settings.DATABASE_URL, echo=True, future=True)
+
+# ⬇️ DI-ready сессионная фабрика
+async_session = sessionmaker(  # type: ignore
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+)
+
+
+# ⬇️ Провайдер для Depends(get_session)
+async def get_session() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session() as session:
+        yield session
--- app/routes/health.py
+++ app/routes/health.py
@@ -1,4 +1,9 @@
 from fastapi import APIRouter
+from typing import Annotated
+from sqlmodel import select
+from sqlalchemy.ext.asyncio import AsyncSession # noqa
+from fastapi import Depends
+from app.db import get_session
 
 router = APIRouter(prefix="/health")
 
@@ -6,3 +11,12 @@ router = APIRouter(prefix="/health")
 @router.get("/ping")
 async def pong():
     return {"ping": "pong"}
+
+
+@router.get("/db")
+async def ping_db(session: Annotated[AsyncSession, Depends(get_session)]):
+    try:
+        await session.execute(select(1))
+        return {"status": "ok", "message": "База данных доступна"}
+    except Exception as e:
+        return {"status": "error", "message": str(e)}
